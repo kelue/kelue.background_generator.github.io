51c9ba6fb38f882f3eaa7a2b1866554fac5f6449
kelue <austincmoon5@gmail.com>
2020-12-03 13:34:16 +0100

    CSS background generator app

---
+++ script.js
@@ -0,0 +1,21 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("root");
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";  // background: linear-gradient(to right, red , yellow)
+
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+function printCss(){
+    var css1 = color1.value;
+    var css2 = color2.value;
+
+    css.innerHTML("background: linear-gradient(to right, CSS, yellow)")
+}
